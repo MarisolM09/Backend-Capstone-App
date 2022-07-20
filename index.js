9d8f86252f97761249fc9b9f18db62820286906f
MarisolM09 <mmendez9980@gmail.com>
2022-07-19 21:07:39 -0500

    mysql connection

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 
 const app = express();
-
 const PORT = 5000;
+
+app.use(express.json());
+
 
 app.listen(PORT, () => console.log(`I am listening on port ${PORT}`))
